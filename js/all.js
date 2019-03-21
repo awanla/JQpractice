49d2e31fc50ecc137d8d4f24a6d5f53ca9aa5e9a
awanla <48355798+awanla@users.noreply.github.com>
2019-03-21 15:38:48 +0800

    上傳六角學院jq最終作業

---
+++ js/all.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    $('.click_menu').click(function (e) { 
+        e.preventDefault();
+        $('.product_menu').slideToggle(1000);
+    });
+    var mySwiper = new Swiper('.swiper-container', {
+        // 可選參數
+        loop: true,
+
+        // 如果需要分頁設置
+        pagination: {
+            el: '.swiper-pagination',
+        },
+
+        // 上一個/下一個 按鈕
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+    
+    $('.top-icon').click(function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+
+    $('body').scroll(function () { 
+        if ($('body').scrollTop() > 0)
+            $('.top-icon').hide();
+        else
+            $('.top-icon').hide();
+});
+    });
+    
